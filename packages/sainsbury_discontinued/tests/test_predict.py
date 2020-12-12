01bc18bb33e2fc778204e8ef0f40c1c0f2434c02
zakarialazilitni <zakaria.alzilitni@outlook.com>
2020-12-12 21:12:46 +0000

    test updatk

--- packages/sainsbury_discontinued/tests/test_predict.py
+++ packages/sainsbury_discontinued/tests/test_predict.py
@@ -15,7 +15,7 @@ def test_make_single_prediction():
     # Then
     assert subject is not None
     assert isinstance(subject.get('predictions')[0], int)
-    assert subject.get('predictions')[0] == 0
+    assert math.ceil(subject.get('predictions')[0]) == 1
 
 
 def test_make_multiple_predictions():
